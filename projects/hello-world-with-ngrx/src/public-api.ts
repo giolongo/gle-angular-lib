d27c5842af2737a8f5123a45b691133439a6418b
giovanni.longo <giovanni.longo@rbtech.dev>
2025-03-28 13:53:54 +0100

    fix: correct export in public api

--- projects/hello-world-with-ngrx/src/public-api.ts
+++ projects/hello-world-with-ngrx/src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/hello-world-with-ngrx.service';
 export * from './lib/hello-world-with-ngrx.component';
+export * from './lib/hello-world-with-ngrx.module';
+export * from './lib/+state/hello-world.facade';
